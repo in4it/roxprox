6247a23d2d28f0701658a5ad550cc17239d3197f
Edward Viaene <ward@in4it.io>
2019-09-11 16:51:00 +0300

    datadog integration

--- terraform/datadog.tf
+++ terraform/datadog.tf
@@ -36,7 +36,7 @@ resource "aws_ecs_service" "datadog" {
 
   network_configuration {
     subnets          = var.subnets
-    security_groups  = [aws_security_group.datadog[0].id]
+    security_groups  = [aws_security_group.roxprox-datadog[0].id]
     assign_public_ip = false
   }
 
