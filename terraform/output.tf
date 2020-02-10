cdf1b1504c3c9deb59c2ba20e59938ff8d2df91e
Lucas <58938251+lucas-in4it@users.noreply.github.com>
2020-02-10 08:53:18 +0100

    add lb arn to output (#37)

--- terraform/output.tf
+++ terraform/output.tf
@@ -6,6 +6,10 @@ output "lb-zone-id" {
   value = aws_lb.lb.zone_id
 }
 
+output "lb-arn" {
+  value = aws_lb.lb.arn
+}
+
 output "roxprox-envoy-sg" {
-   value = var.loadbalancer == "alb" ? aws_security_group.roxprox-envoy-alb[0].id : aws_security_group.roxprox-envoy-nlb[0].id
+  value = var.loadbalancer == "alb" ? aws_security_group.roxprox-envoy-alb[0].id : aws_security_group.roxprox-envoy-nlb[0].id
 }
