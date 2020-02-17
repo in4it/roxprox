ecc857e02a55645efb4caa70fa2a14fa62599be3
Lucas <lukasz.rohde@in4it.io>
2020-02-17 09:03:12 +0100

    add arn suffix to use with cloudwatch metrics

--- terraform/output.tf
+++ terraform/output.tf
@@ -10,6 +10,10 @@ output "lb-arn" {
   value = aws_lb.lb.arn
 }
 
+output "lb-arn-suffix" {
+  value = aws_lb.lb.arn_suffix
+}
+
 output "roxprox-envoy-sg" {
   value = var.loadbalancer == "alb" ? aws_security_group.roxprox-envoy-alb[0].id : aws_security_group.roxprox-envoy-nlb[0].id
 }
