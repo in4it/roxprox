bced26f44624f73ff6ed636255b7cafad5544e7c
Pablo Serrano <pablo@pabloserrano.cc>
2020-05-15 12:27:33 +0200

    S3 public access block

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -3,6 +3,16 @@ resource "aws_s3_bucket" "roxprox" {
   acl    = "private"
 }
 
+resource "aws_s3_bucket_public_access_block" "roxpmox" {
+  bucket = "${aws_s3_bucket.roxprox.id}"
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+
+}
+
 resource "aws_s3_bucket_notification" "roxprox-notification" {
   bucket = aws_s3_bucket.roxprox.id
 
